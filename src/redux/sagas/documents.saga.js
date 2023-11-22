ecec4b571d14c359e01fb4a9b0550e5f76640687
JD <nafziger10@gmail.com>
2023-11-22 12:07:46 -0600

    Initialize bundledata

---
+++ src/redux/sagas/documents.saga.js
@@ -0,0 +1,187 @@
+import axios from "axios";
+import { put, takeLatest } from "redux-saga/effects";
+
+// Documents Saga: will be fired on "FETCH_DOCUMENTS" actions
+function* fetchDocuments(action) {
+  try {
+    const config = {
+      headers: { "Content-Type": "application/json" },
+      withCredentials: true,
+    };
+    const response = yield axios.get(
+      "/api/document/get-my-documents/" + action.payload,
+      config
+    );
+    // console.log("will this CONSOLE LOG");
+
+    yield put({ type: "SET_RESULTS", payload: response.data });
+    // console.log("setting REDUCER");
+  } catch (error) {
+    console.log("User get request failed", error);
+  }
+}
+
+function* fetchDocumentNames() {
+  try {
+    const config = {
+      headers: { "Content-Type": "application/json" },
+      withCredentials: true,
+    };
+
+    const response = yield axios.get("/api/document/names", config);
+
+    yield put({ type: "SET_DOCUMENT_NAMES", payload: response.data });
+    yield put({ type: "UNSET_RESULTS" });
+  } catch (error) {
+    console.log("User get request failed", error);
+  }
+}
+
+function* deleteDocument(action) {
+  try {
+    const config = {
+      headers: { "Content-Type": "application/json" },
+      withCredentials: true,
+    };
+    // console.log(action.payload, 'DOCUMENT.PAYLOAD');
+
+    const response = yield axios.delete(
+      "/api/document/" + action.payload,
+      config
+    );
+
+    yield put({ type: "FETCH_DOCUMENTS" });
+  } catch (error) {
+    console.log("User get request failed", error);
+  }
+}
+
+function* uploadUserDocuments(action) {
+  const doc = action.payload.doc;
+  const standardId = action.payload.standardId;
+  try {
+    const data = new FormData();
+    data.append("file", doc);
+    data.append("upload_preset", "intelliU");
+
+    const res = yield axios.post(
+      `https://api.cloudinary.com/v1_1/dae8japsd/upload`,
+      data
+    );
+    const document_link = res.data.secure_url;
+    const docSubmission = {
+      document_name: doc.name,
+      time_stamp: doc.lastModified,
+      document_link,
+    };
+    // console.log("doc Submission", docSubmission);
+    yield axios.post("/api/document/lowspec", docSubmission);
+    const recentDoc = yield axios.get("/api/document/lowspec/recent");
+    // console.log(
+    //   "RECENT DOC BACK:",
+    //   recentDoc.data[0].id,
+    //   "standard:",
+    //   standardId
+    // );
+    yield put({
+      type: "FETCH_DOCS_TO_SUBMIT",
+      payload: { docA: standardId, docB: recentDoc.data[0].id },
+    });
+
+    // console.log("THEY FAILED");
+  } catch (error) {
+    console.log("Error uploading Document", doc.name, "error:", error);
+  }
+}
+function* fetchDocsToSubmit(action) {
+  // action.payload = {docA: 4, docB: 17, }
+  console.log("FETCHING DOCS", action.payload);
+  try {
+    // /example?param1=value1&param2=value2
+    const response = yield axios.get(
+      `/api/document/docsToSubmit?docA=${action.payload.docA}&docB=${action.payload.docB}`
+    );
+    console.log("bundled and ready:", response.data);
+
+    const name1 = response.data.result1.device_name;
+    const name2 = response.data.result2.document_name;
+    console.log("name1 and two:", name1, name2);
+    // SET THEM IN A REDUCER TO DISPLAY ON REVIEW PAGE
+    yield put({
+      type: "SUBMITTED_DOCS",
+      payload: {
+        docA: name1,
+        docB: name2,
+      },
+    });
+  } catch (error) {
+    console.log("Error fetching docs to submit:");
+  }
+}
+function* uploadAdminDocument(action) {
+  const doc = action.payload;
+  try {
+    const data = new FormData();
+    data.append("file", doc);
+    data.append("upload_preset", "intelliU");
+
+    const res = yield axios.post(
+      `https://api.cloudinary.com/v1_1/dae8japsd/upload`,
+      data
+    );
+    const document_link = res.data.secure_url;
+    const docSubmission = {
+      document_name: doc.name,
+      time_stamp: doc.lastModified,
+      document_link,
+    };
+    console.log("doc Submission", docSubmission);
+    // yield axios.post("/api/document/lowspec", docSubmission);
+    yield axios.post("/api/document/highspec", docSubmission);
+    put({ type: "FETCH_STANDARDS" });
+  } catch (error) {
+    console.log("Error uploading Document", doc.name, "error:", error);
+  }
+}
+function* getAllUserDocs() {
+  try {
+    const result = yield axios.get("/api/document/lowspec");
+    // console.log("RESULT FROM SAGAS USER_DOCUMENTS GET ALL:", result);
+    yield put({ type: "SET_USER_DOCS", payload: result.data });
+    yield put({ type: "SET_SELECTED", payload: result.data[0] })
+  } catch (error) {
+    console.log("Get All User Docs error:", error);
+  }
+}
+function* fetchStandards() {
+  try {
+    const result = yield axios.get("/api/document/highspec");
+    // console.log("RESULT FROM GET STANDARDS:", result.data);
+    yield put({ type: "SET_STANDARDS", payload: result.data });
+  } catch (error) {
+    console.log("error fetching standards:", error);
+  }
+}
+//TODO STRETCH: Actually send documents to IntelliU
+function* docsToIntelliU() {
+  try {
+    console.log("DOCUMENTS HAVE BEEN SENT.... dummy place holder...");
+  } catch (error) {
+    console.log("error submitting docs to intelliU", error);
+  }
+}
+function* documentsSaga() {
+  yield takeLatest("FETCH_DOCUMENTS", fetchDocuments);
+  yield takeLatest("FETCH_DOCUMENT_NAMES", fetchDocumentNames);
+  yield takeLatest("DELETE_DOCUMENT", deleteDocument);
+  yield takeLatest("UPLOAD_USER_DOCS", uploadUserDocuments);
+  yield takeLatest("DOCS_TO_INTELLIU", docsToIntelliU);
+  yield takeLatest("FETCH_STANDARDS", fetchStandards);
+  yield takeLatest("UPLOAD_ADMIN_DOCUMENT", uploadAdminDocument);
+  yield takeLatest("FETCH_DOCS_TO_SUBMIT", fetchDocsToSubmit);
+  // yield takeLatest("UPDATE_JSON",updateJSON);
+  yield takeLatest("FETCH_USER_DOCS",getAllUserDocs);
+
+}
+
+export default documentsSaga;
